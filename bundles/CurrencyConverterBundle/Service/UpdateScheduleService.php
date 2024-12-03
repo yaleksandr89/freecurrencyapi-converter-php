6c476a8a682b6c48319149d87f7cffac5d5f16c2
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2024-12-04 00:19:48 +0300

    added  and 'update_frequency.hours' with pluralization rules (hour|hours)

--- bundles/CurrencyConverterBundle/Service/UpdateScheduleService.php
+++ bundles/CurrencyConverterBundle/Service/UpdateScheduleService.php
@@ -4,17 +4,23 @@ namespace Bundles\CurrencyConverterBundle\Service;
 
 
 use DateTime;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 readonly class UpdateScheduleService
 {
     public function __construct(
-        private int $updateIntervalHours
+        private int $updateIntervalHours,
+        private TranslatorInterface $translator
     ) {
     }
 
     public function getUpdateFrequency(): string
     {
-        return "{$this->updateIntervalHours} часа";
+        return $this->translator->trans(
+            'update_frequency.hours',
+            ['%count%' => $this->updateIntervalHours],
+            'CurrencyConverterBundle'
+        );
     }
 
     public function calculateNextUpdate(DateTime $lastUpdate): DateTime
